e53edc1a782b083190b6e7eba28e0ca1c77e6475
Meagan Roberts <reaganmoberts@gmail.com>
2022-03-01 09:20:38 -0700

    add pantry view, add recipe view, add pantry add view

---
+++ app/src/main/java/com/example/pocketpantry/PantryView.java
@@ -0,0 +1,14 @@
+package com.example.pocketpantry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PantryView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantry_view);
+    }
+}
