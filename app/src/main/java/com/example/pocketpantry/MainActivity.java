c5e4ed772eac57693dc56a310871cf5e630e0413
Meagan Roberts <reaganmoberts@gmail.com>
2022-03-08 20:14:54 -0700

    add new pantry add view

--- app/src/main/java/com/example/pocketpantry/MainActivity.java
+++ app/src/main/java/com/example/pocketpantry/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.pocketpantry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button pantryButton, recipeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pantryButton = findViewById(R.id.mainPantryButton);
+        recipeButton = findViewById(R.id.mainRecipeButton);
+
+        pantryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PantryView.class);
+                startActivity(intent);
+            }
+        });
+
+        recipeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RecipeView.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/pocketpantry/PantryView.java
+++ app/src/main/java/com/example/pocketpantry/PantryView.java
@@ -2,13 +2,31 @@ package com.example.pocketpantry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PantryView extends AppCompatActivity {
+    private Button backButton;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantry_view);
+
+        backButton = findViewById(R.id.pantryBackButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PantryView.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
