e4f0b98e8b1d4f786d45240a2787f61e23c887ed
Meagan Roberts <reaganmoberts@gmail.com>
2022-03-09 21:39:09 -0700

    connect add and back buttons

--- app/src/main/java/com/example/pocketpantry/PantryView.java
+++ app/src/main/java/com/example/pocketpantry/PantryView.java
@@ -2,13 +2,39 @@ package com.example.pocketpantry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class PantryView extends AppCompatActivity {
+    private Button addButton;
+    private ImageButton backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantry_view);
+
+        addButton = findViewById(R.id.pantryAddButtton);
+        backButton = findViewById(R.id.backButton);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PantryView.this, PantryAddView.class);
+                startActivity(intent);
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PantryView.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
