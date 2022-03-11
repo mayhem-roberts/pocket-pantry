0085a3b764e47b8f91f050183e60c9f79b0e3271
Meagan Roberts <reaganmoberts@gmail.com>
2022-03-11 15:58:20 -0700

    remove action bar

--- app/src/main/java/com/example/pocketpantry/MainActivity.java
+++ app/src/main/java/com/example/pocketpantry/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
 
         pantryButton = findViewById(R.id.mainPantryButton);
