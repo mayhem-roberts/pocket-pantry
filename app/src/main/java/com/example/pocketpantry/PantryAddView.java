65fdfbb001aeba3c47f522d3b29b3d02f9e4044f
John Cornia <john.cornia.1@gmail.com>
2022-03-21 09:58:40 -0600

    getting text from pantry add view

--- app/src/main/java/com/example/pocketpantry/PantryAddView.java
+++ app/src/main/java/com/example/pocketpantry/PantryAddView.java
@@ -7,10 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class PantryAddView extends AppCompatActivity {
     private Button addButton;
     private ImageButton backButton;
+    private TextView quantity, weight;
+    private Presenter presenter;
+    private Spinner pantryItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +23,22 @@ public class PantryAddView extends AppCompatActivity {
         setContentView(R.layout.activity_pantry_add_view);
         addButton = findViewById(R.id.pantryAddButtton);
         backButton = findViewById(R.id.backButton2);
+        quantity = findViewById(R.id.quantity).getText();
+        weight = findViewById(R.id.weight);
+        pantryItem = findViewById(R.id.pantryItems);
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(PantryAddView.this,PantryView.class);
                 startActivity(intent);
+            }
+        });
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                presenter.onClickCreatePantryItem(PantryAddView.this, pantryItem, quantity, weight);
             }
         });
     }
